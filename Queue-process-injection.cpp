7c15e2517ff02de8533ec6df654bcc0e29610001
Gele Hasan <111875403+gelehasan@users.noreply.github.com>
2025-05-12 13:17:13 +0200

    defined libraries and variables

--- Queue-process-injection.cpp
+++ Queue-process-injection.cpp
@@ -1,1 +1,12 @@
+#include <iostream>
+#include <Windows.h>
+#include <TlHelp32.h>
+#include <cstring>
+using namespace std;
 
+
+unsigned char shellCode[] = {
+	0x90, 0x20, 0x24
+};
+
+unsigned shellCodeLength = sizeof(shellCode);
