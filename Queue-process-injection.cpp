de99cfe4a6c317a47eb5401d09b82f451bdb124b
Gele Hasan <111875403+gelehasan@users.noreply.github.com>
2025-05-12 13:17:33 +0200

    Implemented function for finding process Id

--- Queue-process-injection.cpp
+++ Queue-process-injection.cpp
@@ -10,3 +10,31 @@ unsigned char shellCode[] = {
 };
 
 unsigned shellCodeLength = sizeof(shellCode);
+
+
+DWORD findProcessID(const wchar_t* processName) {
+	DWORD processId = 0;
+
+	HANDLE processSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+
+	if (processSnapshot == INVALID_HANDLE_VALUE) {
+		cout << "Couldnt get process handles";
+		return 0;
+	}
+
+	PROCESSENTRY32 pe;
+	pe.dwSize = sizeof(pe);
+
+	if (Process32First(processSnapshot, &pe)) {
+		do {
+			if (wcscmp( pe.szExeFile, processName) ==0) {
+				processId = pe.th32ProcessID;
+				break;
+			}
+		} while (Process32Next(processSnapshot, &pe));
+	}
+
+	CloseHandle(processSnapshot);
+
+	return processId;
+}
