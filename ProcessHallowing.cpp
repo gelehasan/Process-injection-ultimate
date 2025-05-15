219bbd69fba887d4eadbe217844bb582572edd8c
Gele Hasan <111875403+gelehasan@users.noreply.github.com>
2025-05-15 15:54:56 +0200

    Imported needed libraries

--- ProcessHallowing.cpp
+++ ProcessHallowing.cpp
@@ -1,1 +1,9 @@
+#include <Windows.h>
+#include <winternl.h>
+#include <iostream>
+#include <fstream>
 
+#pragma comment(lib, "ntdll.lib")
+
+typedef NTSTATUS(NTAPI* pNtUnmapViewOfSection)(HANDLE, PVOID);
+using namespace std;
