f9854269a983897b26cd20e541d7ffecc34aafb6
Gele Hasan <111875403+gelehasan@users.noreply.github.com>
2025-05-17 23:42:13 +0200

    defined variables for the process

--- ProcessHallowing.cpp
+++ ProcessHallowing.cpp
@@ -7,3 +7,16 @@
 
 typedef NTSTATUS(NTAPI* pNtUnmapViewOfSection)(HANDLE, PVOID);
 using namespace std;
+
+
+int main() {
+	PROCESS_INFORMATION pi;
+	STARTUPINFO si;
+	si.dwXSize = sizeof(si);
+	// The application we are targeting
+	const char* targetProcessPath = "C:\\Windows\\System32\\notepad.exe";
+
+	const char* payloadBuffer = "Running under malicious";
+
+
+}
