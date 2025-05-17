752732a098b11882f7d4d1b2ed89ae617e67d9ee
Gele Hasan <111875403+gelehasan@users.noreply.github.com>
2025-05-17 23:42:47 +0200

    Getting malicious code meta data

--- ProcessHallowing.cpp
+++ ProcessHallowing.cpp
@@ -17,6 +17,17 @@ int main() {
 	const char* targetProcessPath = "C:\\Windows\\System32\\notepad.exe";
 
 	const char* payloadBuffer = "Running under malicious";
-
+	
+	// Pointing to the malicious program header and its meta data
+	//PE header contains information for the operating system how to load and run the program
+	// The metafile of a valid exe program, most important field in heere is e_lfanew
+	PIMAGE_DOS_HEADER Dos_Header = (PIMAGE_DOS_HEADER)payloadBuffer;
+	
+	// Ntheaders or PE header contains valuable information 
+	PIMAGE_NT_HEADERS ntHeaders = (PIMAGE_NT_HEADERS)(payloadBuffer + Dos_Header->e_lfanew);
+	// Where it wants to be loaded in memory address
+	LPVOID Image_base = (LPVOID)ntHeaders->OptionalHeader.ImageBase;
+	// The amount of memory space it needs
+	SIZE_T imageSize = ntHeaders->OptionalHeader.SizeOfImage;
 
 }
